3aa5ffbba5bd1c902864f4fa4f3f9bbd0c0bcb0b
Kat Marchán <kzm@zkat.tech>
2021-09-15 19:00:33 -0700

    fix(conditions): allow fallthrough when nested checks are false
    Fixes: https://github.com/zkat/supports-hyperlinks/issues/1

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,34 @@ pub use atty::Stream;
 pub fn supports_hyperlinks() -> bool {
     if std::env::var("DOMTERM").is_ok() {
         // DomTerm
-        true
-    } else if let Ok(version) = std::env::var("VTE_VERSION") {
+        return true;
+    }
+
+    if let Ok(version) = std::env::var("VTE_VERSION") {
         // VTE-based terminals above v0.50 (Gnome Terminal, Guake, ROXTerm, etc)
-        version.parse().unwrap_or(0) >= 5000
-    } else if let Ok(program) = std::env::var("TERM_PROGRAM") {
-        matches!(
+        if version.parse().unwrap_or(0) >= 5000 {
+            return true;
+        }
+    }
+
+    if let Ok(program) = std::env::var("TERM_PROGRAM") {
+        if matches!(
             &program[..],
             "Hyper" | "iTerm.app" | "terminology" | "WezTerm"
-        )
-    } else if let Ok(term) = std::env::var("TERM") {
-        // Kitty
-        matches!(&term[..], "xterm-kitty")
-    } else {
-        // Windows Terminal
-        std::env::var("WT_SESSION").is_ok() ||
-            // Konsole
-            std::env::var("KONSOLE_VERSION").is_ok()
+        ) {
+            return true;
+        }
     }
+
+    if let Ok(term) = std::env::var("TERM") {
+        // Kitty
+        if matches!(&term[..], "xterm-kitty") {
+            return true;
+        }
+    }
+
+    // Windows Terminal and Konsole
+    std::env::var("WT_SESSION").is_ok() || std::env::var("KONSOLE_VERSION").is_ok()
 }
 
 pub fn on(stream: Stream) -> bool {
