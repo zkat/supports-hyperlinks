07fe45349c00f720adaceee17bfefd10bece3bd6
George Pollard <porges@porg.es>
2023-03-13 21:04:16 +0000

    Match public interface to supports-color

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 #![doc = include_str!("../README.md")]
 
+/// possible stream sources
+#[derive(Clone, Copy, Debug)]
+pub enum Stream {
+    Stdout,
+    Stderr,
+}
+
 /// Returns true if the current terminal, detected through various environment
 /// variables, is known to support hyperlink rendering.
 pub fn supports_hyperlinks() -> bool {
@@ -40,8 +47,16 @@ pub fn supports_hyperlinks() -> bool {
     std::env::var("WT_SESSION").is_ok() || std::env::var("KONSOLE_VERSION").is_ok()
 }
 
+fn is_a_tty(stream: Stream) -> bool {
+    use is_terminal::*;
+    match stream {
+        Stream::Stdout => std::io::stdout().is_terminal(),
+        Stream::Stderr => std::io::stderr().is_terminal(),
+    }
+}
+
 /// Returns true if `stream` is a TTY, and the current terminal
 /// [supports_hyperlinks].
-pub fn on(stream: &dyn is_terminal::IsTerminal) -> bool {
-    (std::env::var("FORCE_HYPERLINK").is_ok() || stream.is_terminal()) && supports_hyperlinks()
+pub fn on(stream: Stream) -> bool {
+    (std::env::var("FORCE_HYPERLINK").is_ok() || is_a_tty(stream)) && supports_hyperlinks()
 }
